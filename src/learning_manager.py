60f5256c3b88571db1df9291f1cd8850064c8048
alexis <alexis.jacq@gmail.com>
2015-07-14 14:42:19 +0200

    read/save in datasets

--- src/learning_manager.py
+++ src/learning_manager.py
@@ -9,7 +9,13 @@ import logging; shapeLogger = logging.getLogger("shape_logger")
 import os.path
 from allograph import stroke
 
+# global variables :
+#-------------------
+
 alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+# learning_manager class :
+#-------------------------
 
 class LearningManager():
 
@@ -17,8 +23,8 @@ class LearningManager():
         self.generate_path = generate_path
         self.demo_path = demo_path
         self.robot_path = robot_path
+        self.robot_data = read_data(self.robot_path,0)
         self.generate_letters('last_state')
-        self.word_to_learn = ""
 
     def generate_word(self, word):
         generated_word = []
@@ -40,13 +46,49 @@ class LearningManager():
             for letter,stroke in demonstration:
                 learned_stroke = stroke.midway(stroke, self.generated_letters[letter])
                 self.generated_letters[letter] = learned_stroke
+                save_learned_allograph(self.robot_data, letter, learned_stroke)
         #if mode = 'PCA' 
         #if mode = 'sigNorm' (mixture of sigma-log-normal)
         #if mode = 'CNN' (1-D convolutionnal neural networks)
 
     def read_last_line(self, letter):
+        return self.robot_data[letter][-1]
 
+# static functions :
+#-------------------
 
+def read_data(datasetDirectory, lines_to_jump):
+    data_letters = {}
+    datasets = glob.glob(datasetDirectory + '/*.dat')
+    for dataset in datasets:
+        name = os.path.splitext(os.path.basename(dataset))[0]
+        if name in alphabet:
+            try:
+                with open(dataset, 'r') as data:
+                    i=0
+                    for line in data.readlines():
+                        i+=1
+                        if i>lines_to_jump:
+                            shape = literal_eval('['+line.replace(' ',', ')+']')
+                            shape = np.reshape(shape, (-1, 1))
+                            data_stroke = Stroke()
+                            data_stroke.stroke_from_xxyy(np.reshape(shape,len(shape)))
+                            data_stroke.uniformize()
+                            data_stroke.normalize()
+                            data_letters.setdefault(name,[]).append(data_stroke)
+            except IOError:
+                raise RuntimeError("no reading permission for file"+dataset )
+    return data_letters
 
+def save_learned_allograph(datasetDirectory, letter, stroke):
+    dataset = datasetDirectory + '/' + letter + '.dat'
+    if not os.path.exists(dataset):
+        raise RuntimeError("path to dataset"+dataset+"not found")
+    try:
+        with open(dataset, "a") as f:
+            f.write(' '.join(map(str,stroke)).replace('[','').replace(']','').replace('\n',''))
+            f.write('\n')
+    except IOError:
+        raise RuntimeError("no writing permission for file"+dataset)
 
 
